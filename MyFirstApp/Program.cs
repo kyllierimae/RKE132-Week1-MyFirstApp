80493f968a00643cbc613761317b62897a24ca2c
kuerim <kuerim@taltech.ee>
2023-09-24 13:50:21 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// This is a comment.
+// Ask name and say hello.
+Console.WriteLine("Enter your name:");
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+Console.WriteLine($"Hello, {userName} !");
